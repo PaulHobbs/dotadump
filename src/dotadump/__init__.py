e70966abc429f7ae41519ee546a4d0024a067f01
Paul Hobbs <phobbs@linkedin.com>
2014-03-03 10:52:23 -0800

    Matches api

--- src/dotadump/__init__.py
+++ src/dotadump/__init__.py
@@ -1,6 +1,6 @@
 import json
 import os
 
-with open(os.path.dirname(__file__) + '../../config/master.json') as fp:
+with open(os.path.join(os.path.dirname(__file__),
+                       '../../configs/master.json')) as fp:
   config = json.load(fp)
-
--- src/dotadump/api.py
+++ src/dotadump/api.py
@@ -1,1 +1,26 @@
 import requests
+from dotadump import config
+
+
+ROOT = "https://api.steampowered.com/IDOTA2Match_570"
+
+
+def decorate(url):
+  return url + '/V001'
+
+
+def matches(**kwargs):
+  kwargs = dict(kwargs, key=config['API_KEY'])
+  results_remaining = 1
+  while results_remaining:
+    result = requests.get(decorate(ROOT + '/GetMatchHistory'), params=kwargs).json()
+    results_remaining = result['results_remaining']
+    for match in result['matches']:
+      yield match
+    kwargs['start_at_match_id'] = match['match_id'] - 1
+
+
+def details(match_id, **kwargs):
+  kwargs = dict(kwargs, key=config['API_KEY'])
+  return requests.get(decorate(ROOT + '/GetMatchDetails'),
+                      params=dict(kwargs, match_id=match_id)).json()
